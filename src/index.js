d929c1ab9bb225cfef473f00bd3e22228dfb0c55
abdulla <abdullaabdullaev0803@gmail.com>
2023-04-19 09:55:38 +0300

    add merge sort function

---
+++ src/index.js
@@ -0,0 +1,70 @@
+// eslint-disable-next-line max-classes-per-file
+class Node {
+    constructor(attr, leftChild, rightChild) {
+        this.attr = attr
+        this.leftChild = leftChild
+        this.rightChild = rightChild
+    }
+}
+class Tree {
+    constructor(array, root) {
+        this.root = root
+    }
+}
+const arrayNumbers = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+
+const duplicatesRemoved = function removeDuplicates(array) {
+    return Array.from(new Set(array))
+}
+
+const newArray = duplicatesRemoved(arrayNumbers)
+
+function merge(leftArray, rightArray) {
+    let leftIndex = 0
+    let rightIndex = 0
+    const result = []
+    let resultIndex = 0
+
+
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+        if (leftArray[leftIndex] < rightArray[rightIndex]) {
+            result[resultIndex] = (leftArray[leftIndex])
+            leftIndex++
+            resultIndex++
+        }
+        else {
+            result[resultIndex] = (rightArray[rightIndex])
+            rightIndex++
+            resultIndex++
+        }
+    }
+    while (leftIndex < leftArray.length) {
+        result[resultIndex] = leftArray[leftIndex];
+        leftIndex++;
+        resultIndex++;
+    }
+
+    // Append remaining elements from rightArray
+    while (rightIndex < rightArray.length) {
+        result[resultIndex] = rightArray[rightIndex];
+        rightIndex++;
+        resultIndex++;
+    }
+    console.log(result);
+    return result
+}
+function mergeSort(array) {
+    if (array.length <= 1) { return array }
+    const mid = Math.floor(array.length / 2);
+    const leftHalf = array.slice(0, mid);
+    const rightHalf = array.slice(mid);
+
+    const sortedLeft = mergeSort(leftHalf);
+    const sortedRight = mergeSort(rightHalf);
+
+    return merge(sortedLeft, sortedRight)
+}
+mergeSort(newArray)
+
+
+
